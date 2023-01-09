0edfb2ab42966de4ed56fd32b34bb1d84bcb3d67
michaellacey <mikelacey123123@gmail.com>
2023-01-09 10:44:01 -0500

    finished!

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -29,8 +29,49 @@ export default function HomePage() {
 
     return (
         <div className="homepageMainDiv">
+            <h1 id="hpH1"> Hottest Electronics </h1>
 
-        
+            <div className="layer1">
+
+                <div className="product1">
+                    <p className="hpProductName">{products[0]?.name}</p>
+                    <p className="hpProductName">${products[0]?.price}</p>
+                    <Link to={`/departments/${products[0]?.department_id}/${products[0]?.id}`} className="hpImgLink">
+                    <img className="hpImg" src={products[0]?.image_url1} alt="" />
+                    </Link>
+                </div>
+                        
+
+                <div className="product2">
+                    <p className="hpProductName">{products[5]?.name}</p>
+                    <p className="hpProductName">${products[5]?.price}</p>
+                    <Link to={`/departments/${products[5]?.department_id}/${products[5]?.id}`} className="hpImgLink">
+                    <img className="hpImg" src={products[5]?.image_url1} alt="" />
+                    </Link>
+                </div>
+
+            </div>
+
+            <div className="layer2">
+                <div className="product1">
+                    <p className="hpProductName">{products[10]?.name}</p>
+                    <p className="hpProductName">${products[10]?.price}</p>
+                    <Link to={`/departments/${products[10]?.department_id}/${products[10]?.id}`} className="hpImgLink">
+                    <img className="hpImg" src={products[10]?.image_url1} alt="" />
+                    </Link>
+                </div>
+
+                <div className="product2">
+                    
+                    <p className="hpProductName">{products[9]?.name}</p>
+                    <p className="hpProductName">${products[9]?.price}</p>
+                    <Link to={`/departments/${products[9]?.department_id}/${products[9]?.id}`} className="hpImgLink">
+                    <img className="hpImg" src={products[9]?.image_url1} alt="" />
+                    </Link>
+                </div>
+
+            </div>
+
             <h1 id="catH2"> Departments </h1>
             <div className="departments">
                 {departmentsArr && departmentsArr.map(ele => (
